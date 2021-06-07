b33d4c0259802d0c1f485769fee27cb296b3e0af
Carolyn Berard <carolynmberard@gmail.com>
2021-06-07 11:54:17 -0500

    Completed all backend routes & database models

--- server/api/breads.js
+++ server/api/breads.js
@@ -1,1 +1,56 @@
 const router = require('express').Router()
+const { Bread } = require('../db')
+
+// GET all breads from the database
+router.get('/', async (req, res, next) => {
+  try {
+    const breads = await Bread.findAll()
+    res.send(breads)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// GET a single bread from the database
+router.get('/:breadId', async (req, res, next) => {
+  try {
+    const bread = await Bread.findByPk(req.params.breadId)
+    res.send(bread)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// POST a new bread to the database
+router.post('/', async (req, res, next) => {
+  try {
+    const newBread = await Bread.create(req.body)
+    res.json(newBread)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// UPDATE an existing bread in the database
+router.put('/:breadId', async (req, res, next) => {
+  try {
+    const bread = await Bread.findByPk(req.params.breadId)
+    const updatedBread = await bread.update(req.body)
+    res.send(updatedBread)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// DELETE an existing bread from the database
+router.delete('/:breadId', async (req, res, next) => {
+  try {
+  const bread = await Bread.findByPk(req.params.breadId)
+  await bread.destroy()
+  res.send(bread)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
