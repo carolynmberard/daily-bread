9cee10ae4d6f8ad27edb9d02b479d31e988ce084
Carolyn Berard <carolynmberard@gmail.com>
2021-06-07 10:25:47 -0500

    Filled out project directory

---
+++ app/components/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './Navbar'
+import Homepage from './Homepage'
+import AllBreads from './AllBreads'
+import SingleBread from './SingleBread'
+
+const Routes = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/breads" component={AllBreads} />
+          <Route exact path="/breads/:breadId" component={SingleBread} />
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default Routes
---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+// Here we're bringing in our Sequelize instance together with the model.
+// While only one model is currently needed, this format would allow us to more
+// easily add additional models & associations without needed to change the
+// underlying structure too much.
+
+const db = require('./database')
+const Bread = require('./bread')
+
+module.exports = {
+  db,
+  Bread
+}
